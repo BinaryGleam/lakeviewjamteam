ed23ff87c1acfd4f483081e2ae8d2f69cb54ff4f
Lukas TAMAYO <luknight@hotmail.fr>
2022-10-01 22:07:54 +0200

    Shooting working (kinda)

--- LakeView-Jam/Assets/Scripts/PlayerGun.cs
+++ LakeView-Jam/Assets/Scripts/PlayerGun.cs
@@ -12,10 +12,15 @@ public class PlayerGun : MonoBehaviour
     [SerializeField]
     private Light gunLight = null;
 
+    [SerializeField]
+    [Range(0.01f,0.15f)]
+    private float fireVFXDecayDelay = 0.05f;
+    private float chrono = 0f;
+
     void Awake()
     {
         lineRendererRef = GetComponent<LineRenderer>();
-        if (rigidbodyRef == null)
+        if (lineRendererRef == null)
         {
             Debug.LogError(this.name + " script isn't linked to something with a lineRenderer. Script gonna auto destroy");
             Destroy(this);
@@ -31,27 +36,46 @@ public class PlayerGun : MonoBehaviour
     void Update()
     {
         bShooting = Input.GetButtonDown("Fire1");
+        if(bShooting)
+		{
+            chrono = fireVFXDecayDelay;
+		}
+        if(VFXEnableCondition())
+		{
+            chrono -= Time.deltaTime;
+		}
+        lineRendererRef.enabled = VFXEnableCondition();
+        gunLight.enabled = VFXEnableCondition();
     }
 
 	private void FixedUpdate()
 	{
-		if(bShooting)
+        lineRendererRef.SetPosition(0, gunLight.transform.position);
+        if (bShooting)
 		{
-            RaycastHit hitInfos;
-            if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitInfos, Mathf.Infinity))
-			{
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitInfos.distance, Color.yellow);
-                IShootable shotGameobject = hitInfos.collider.GetComponent<IShootable>();
-                if (shotGameobject != null)
-				{
-                    shotGameobject.OnGettingShot();
-				}
-            }
-            else
-            {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            }
-            Debug.Log("Shooting");
+            OnShoot();
 		}
 	}
+
+    private void OnShoot()
+	{
+        RaycastHit hitInfos;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitInfos, Mathf.Infinity))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitInfos.distance, Color.yellow);
+            lineRendererRef.SetPosition(1, hitInfos.point);
+            IShootable shotGameobject = hitInfos.collider.GetComponent<IShootable>();
+            if (shotGameobject != null)
+            {
+                shotGameobject.OnGettingShot();
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            lineRendererRef.SetPosition(1, transform.TransformDirection(Vector3.forward) * 1000);
+        }
+    }
+
+    bool VFXEnableCondition() { return chrono > 0f; }
 }
