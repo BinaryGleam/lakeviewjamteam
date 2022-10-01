4a552dd9cdfc0ea38166dc5f1a4b48e2eca04774
Lukas TAMAYO <luknight@hotmail.fr>
2022-10-02 00:32:25 +0200

    Floating props code done

---
+++ LakeView-Jam/Assets/Scripts/FloatingProp.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingProp : MonoBehaviour
+{
+    private Rigidbody rigidbodyRef = null;
+
+    [SerializeField]
+    private Vector3 linearInitForceMin = Vector3.zero,
+                    linearInitForceMax = Vector3.zero,
+                    angularInitForceMin = Vector3.zero,
+                    angularInitForceMax = Vector3.zero;
+
+    void Awake()
+    {
+        rigidbodyRef = GetComponent<Rigidbody>();
+        if (rigidbodyRef == null)
+        {
+            Debug.LogError(this.name + " script isn't linked to something with a rigidbody. Script gonna auto destroy");
+            Destroy(this);
+        }
+        rigidbodyRef.velocity = new Vector3(Random.Range(linearInitForceMin.x,linearInitForceMax.x),
+                                            Random.Range(linearInitForceMin.y, linearInitForceMax.y),
+                                            Random.Range(linearInitForceMin.z, linearInitForceMax.z));
+        
+        rigidbodyRef.angularVelocity = new Vector3(Random.Range(angularInitForceMin.x, angularInitForceMax.x),
+                                                    Random.Range(angularInitForceMin.y, angularInitForceMax.y),
+                                                    Random.Range(angularInitForceMin.z, angularInitForceMax.z));
+    }
+}
